af42cd760ac9d9b4fe8b6c4095b0e3eda99b475a
abhivij <abhishek.kcvijayan@gmail.com>
2020-08-26 20:41:17 +1000

    normlogCPM for gbm 1

---
+++ preprocessing/preprocessing.R
@@ -0,0 +1,43 @@
+library("XLConnect")
+library("stringr")
+library(dplyr)
+library(tidyr)
+library(edgeR)
+
+
+setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/data")
+
+gbm_1 <- read.table("GBM/1/GSE122488_normalized_microRNA_counts.txt", 
+                    header=TRUE, row.names=1, skip=3)
+
+disease_data <- data.frame(colnames(gbm_1))
+colnames(disease_data) <- 'sample_name'
+disease_data['disease_status'] <- disease_data$sample_name
+levels(disease_data$disease_status) <- c(levels(disease_data$disease_status), 'GBM', 'Control', 'Glioma')
+disease_data$disease_status[grepl('GBM', disease_data$disease_status)] <- 'GBM'
+disease_data$disease_status[grepl('HC', disease_data$disease_status)] <- 'Control'
+disease_data$disease_status[grepl('GII.III', disease_data$disease_status)] <- 'Glioma'
+disease_data$disease_status <- factor(disease_data$disease_status)
+
+# set1 <- subset(disease_data, disease_status == 'Control'    )
+# set2 <- subset(disease_data, disease_status == 'GBM'  )
+# 
+# set = data.frame(rep(0,dim(gbm_1)[2]))
+# colnames(set) = "comparator"
+# rownames(set) = colnames(raw)
+# set[rownames(set1),1] = 1
+# set[rownames(set2),1] = 2
+# x.raw <- gbm_1[ ,which(set > 0)]  
+# groups <- set[which(set > 0),]
+
+gbm_and_control_samples <- disease_data$disease_status == 'Control' | disease_data$disease_status == 'GBM'
+x.raw <- gbm_1[, gbm_and_control_samples] 
+disease_data <- disease_data[gbm_and_control_samples, ]
+
+
+keep <- filterByExpr(x.raw)
+x.filtered <- x.raw[keep, ]
+x.logcpm <- cpm(x.filtered, log=TRUE)
+x.normlogcpm <- scale(x.logcpm)
+
+colnames(x.normlogcpm) <- disease_data$disease_status
