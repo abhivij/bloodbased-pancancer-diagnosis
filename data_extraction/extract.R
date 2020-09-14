d720e597cbc47301622eff4e0b5cf499f08391ba
abhivij <abhishek.kcvijayan@gmail.com>
2020-09-14 20:39:50 +1000

    extract function in separate file

--- data_extraction/extract.R
+++ data_extraction/extract.R
@@ -1,20 +1,5 @@
 library(dplyr)
 
-setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/")
-
-
-#user input start
-phenotype_file <- "phenotype_info/phenotype_GBM1.txt"
-
-read_count_dir_path <- "data/GBM/1"
-read_count_file <- "GSE122488_normalized_microRNA_counts.txt"
-skip_row_count <- 3
-
-classification_criteria <- "GBMvsCont"
-#filter <- expression(Age > 55 & Sex == 'M')
-filter <- expression(TRUE)
-#user input end
-
 extract_data <- function(phenotype_file, read_count_file, read_count_dir_path, skip_row_count, classification_criteria, filter) {
   read_count_file_path <- paste(read_count_dir_path, read_count_file, sep = "/")
   
@@ -35,4 +20,4 @@ extract_data <- function(phenotype_file, read_count_file, read_count_dir_path, s
               quote=FALSE, sep="\t", row.names=FALSE)  
 }
 
-extract_data(phenotype_file, read_count_file, read_count_dir_path, skip_row_count, classification_criteria, filter)
+
