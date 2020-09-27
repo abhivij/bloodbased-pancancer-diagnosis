3830e71b753b6640d9d2fb8864b25d95952d3fa7
abhivij <abhishek.kcvijayan@gmail.com>
2020-09-27 20:36:00 +1000

    a common file for gbm1 pipeline

--- gbm1_pipeline.R
+++ gbm1_pipeline.R
@@ -1,5 +1,21 @@
 setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/")
+source("data_extraction/extract.R")
 source("preprocessing/preprocessing.R")
+
+#user input start
+phenotype_file <- "phenotype_info/phenotype_GBM1.txt"
+
+read_count_dir_path <- "data/GBM/1"
+read_count_file <- "GSE122488_normalized_microRNA_counts.txt"
+skip_row_count <- 3
+
+classification_criteria <- "GBMvsCont"
+#filter <- expression(Age > 55 & Sex == 'M')
+filter <- expression(TRUE)
+#user input end
+
+extract_data(phenotype_file, read_count_file, read_count_dir_path, skip_row_count, classification_criteria, filter)
+
 
 read_count_dir_path <- "data/GBM/1"
 read_count_file_name <- "read_counts.txt"
