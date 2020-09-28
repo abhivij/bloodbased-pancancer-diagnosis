4dd046fdbd48c37559e63df5139dc87b7b9be02e
abhivij <abhishek.kcvijayan@gmail.com>
2020-09-28 14:41:48 +1000

    raw read count dim

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -9,6 +9,8 @@ setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis")
 filter_and_normalize <- function(read_count_dir_path, read_count_file, op_file_name){
   x.raw <- read.table(paste(read_count_dir_path, read_count_file, sep="/"), 
                       header=TRUE, row.names=1)
+  print("raw read count dim")
+  print(dim(x.raw))
   keep <- filterByExpr(x.raw)
   x.filtered <- x.raw[keep, ]
   x.logcpm <- cpm(x.filtered, log=TRUE)
