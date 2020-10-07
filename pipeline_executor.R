38758eb47e58bd01ce53991ba0a4f31b30ac546c
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-07 23:39:48 +1100

    gbmVsglioma for gbm dataset1

--- pipeline_executor.R
+++ pipeline_executor.R
@@ -1,7 +1,7 @@
 setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/")
 source("pipeline.R")
 
-#GBM1
+#GBM1 GBMVsCont
 phenotype_file_name <- "phenotype_info/phenotype_GBM1.txt"
 read_count_dir_path <- "data/GBM/1"
 read_count_file_name <- "GSE122488_normalized_microRNA_counts.txt"
@@ -18,3 +18,12 @@ execute_pipeline(phenotype_file_name = phenotype_file_name,
                  classification_criteria = classification_criteria, filter_expression = filter_expression,
                  classes = classes, dataset_id = dataset_id)
 
+
+#GBM1 GBMvsGlioma
+classification_criteria <- "GBMvsGlioma"
+classes <- c("GBM", "Glioma")
+execute_pipeline(phenotype_file_name = phenotype_file_name,
+                 read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
+                 skip_row_count = skip_row_count,
+                 classification_criteria = classification_criteria, filter_expression = filter_expression,
+                 classes = classes, dataset_id = dataset_id)
