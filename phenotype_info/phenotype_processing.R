2cdb43d8986bf5164023af50996cd4cc5a204d5b
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-09 14:50:29 +1100

    LungCancer1 phenotype creation and execution

--- phenotype_info/phenotype_processing.R
+++ phenotype_info/phenotype_processing.R
@@ -2,6 +2,7 @@ setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/phenotype_info/")
 
 library(dplyr)
 
+#GBM1
 phenotype_GBM1 <- read.table(file = "phenotype_GBM1_raw.txt", header=TRUE)
 phenotype_GBM1_modified <- 
   phenotype_GBM1 %>%
@@ -13,4 +14,16 @@ phenotype_GBM1_modified <-
 write.table(phenotype_GBM1_modified, file = "phenotype_GBM1.txt", quote=FALSE, sep="\t", row.names=FALSE)
 
 
-  
+#LungCancer1  
+setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis")
+lung_1 <- read.table("data/LungCancer/1/GSE111803_Readcount_TPM.txt", header = TRUE)
+phenotype_LungCancer1 <- data.frame(Sample = colnames(lung_1)[c(2:11)])
+phenotype_LungCancer1 <- 
+  phenotype_LungCancer1 %>%
+  mutate(DataSetId = "GSE111803", .after = "Sample") %>%
+  mutate(Biomarker = "EV_miRNA", .after = "DataSetId") %>%  
+  mutate(Technology = "RNASeq", .after = "Biomarker")  
+phenotype_LungCancer1['LUADVsControl'] <- ifelse(
+        sapply(phenotype_LungCancer1$Sample, grepl, pattern='LC', fixed=TRUE),
+        'LUAD', 'Control')
+write.table(phenotype_LungCancer1, file = "phenotype_info/phenotype_LungCancer1.txt", quote=FALSE, sep="\t", row.names=FALSE)
--- pipeline_executor.R
+++ pipeline_executor.R
@@ -27,3 +27,18 @@ execute_pipeline(phenotype_file_name = phenotype_file_name,
                  dataset_id = dataset_id)
 
 
+#LungCancer1
+phenotype_file_name <- "phenotype_info/phenotype_LungCancer1.txt"
+read_count_dir_path <- "data/LungCancer/1"
+read_count_file_name <- "GSE111803_Readcount_TPM.txt"
+skip_row_count <- 0
+classification_criteria <- "LUADVsControl"
+filter_expression <- expression(TRUE)
+dataset_id <- "LungCancer1"
+
+execute_pipeline(phenotype_file_name = phenotype_file_name,
+                 read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
+                 skip_row_count = skip_row_count,
+                 classification_criteria = classification_criteria, filter_expression = filter_expression,
+                 dataset_id = dataset_id)
+
