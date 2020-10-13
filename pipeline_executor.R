375631276f987cfbf725df74c4ea81582dfa8ee0
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-13 14:50:52 +1100

    TEP data run

--- pipeline_executor.R
+++ pipeline_executor.R
@@ -49,9 +49,44 @@ read_count_dir_path <- "data/GBM/TEP"
 read_count_file_name <- "GSE68086_TEP_data_matrix.txt"
 skip_row_count <- 0
 classification_criteria <- "GBMVsHC"
-#filter <- expression(Age > 55 & Sex == 'M')
 filter_expression <- expression(TRUE)
 dataset_id <- "TEP2015"
+
+execute_pipeline(phenotype_file_name = phenotype_file_name,
+                 read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
+                 skip_row_count = skip_row_count,
+                 classification_criteria = classification_criteria, filter_expression = filter_expression,
+                 dataset_id = dataset_id)
+
+
+#TEP2015 NSCLCVsHC
+classification_criteria <- "NSCLCVsHC"
+
+execute_pipeline(phenotype_file_name = phenotype_file_name,
+                 read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
+                 skip_row_count = skip_row_count,
+                 classification_criteria = classification_criteria, filter_expression = filter_expression,
+                 dataset_id = dataset_id)
+
+
+#TEP2015 BreastCancerVsHC
+classification_criteria <- "BCVsHC"
+
+execute_pipeline(phenotype_file_name = phenotype_file_name,
+                 read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
+                 skip_row_count = skip_row_count,
+                 classification_criteria = classification_criteria, filter_expression = filter_expression,
+                 dataset_id = dataset_id)
+
+
+#TEP2017 NSCLCVsNonCancer
+phenotype_file_name <- "phenotype_info/phenotype_TEP2017.txt"
+read_count_dir_path <- "data/LungCancer/TEP"
+read_count_file_name <- "GSE89843_TEP_Count_Matrix.txt"
+skip_row_count <- 0
+classification_criteria <- "NSCLCVsNC"
+filter_expression <- expression(TRUE)
+dataset_id <- "TEP2017"
 
 execute_pipeline(phenotype_file_name = phenotype_file_name,
                  read_count_dir_path = read_count_dir_path, read_count_file_name = read_count_file_name,
