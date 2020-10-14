799e658b3cb0a33595ca05f69b02a82213467de9
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-14 20:38:33 +1100

    corrected lungcancer3 dataset read

--- data/exploration.R
+++ data/exploration.R
@@ -49,7 +49,6 @@ lung_1 <- read.table("LungCancer/1/GSE111803_Readcount_TPM.txt", header = TRUE)
 lung_wb <- loadWorkbook("LungCancer/2/176791_2_supp_4043545_kq2mmk.xlsx", create = FALSE)
 lung_2 <- readWorksheet(lung_wb, sheet="miRNA-Seq data")
 
-lung_3 <- ""
 path <- "LungCancer/3/GSE114711_RAW/"
 file.names <- dir(path, pattern =".txt.gz")
 for(i in 1:length(file.names)){
@@ -57,7 +56,13 @@ for(i in 1:length(file.names)){
   print(sample)
   file_path = paste(path, file.names[i], sep="")
   lung_3_sub <- read.table(file_path, col.names = c("Transcripts", sample))
-  lung_3 <- cbind(lung_3, lung_3_sub)
+  if (i == 1){
+    lung_3 <- lung_3_sub
+  }
+  else{
+    lung_3 <- lung_3 %>% 
+      inner_join(lung_3_sub)
+  }
 }
 
 
