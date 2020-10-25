33f2aa3b871e367afccedc00d6260441fdac6b92
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-26 01:46:56 +1100

    all model bar plot with CI

--- results_analyze.R
+++ results_analyze.R
@@ -11,12 +11,11 @@ model_results <- read.table('model_results.csv', sep = ',', header = TRUE)
 fsm_info <- fsm_info %>%
             select(-starts_with('X'))
 
-model_results <- model_results %>%
-  select(-starts_with('X'))
+# model_results <- model_results %>%
+#   select(-starts_with('X'))
 
 RF_AUC <- model_results %>%
-  filter(Model == 'Random Forest') %>%
-  select(-c('Model', 'Mean_Accuracy'))
+  filter(Model == 'Random Forest')
 
 # datasets <- unique(model_results$DataSetId)
 # fsms <- unique(model_results$FSM)
@@ -27,3 +26,12 @@ all_model_heatmap <- ggplot(model_results, aes(x = FSM, y = DataSetId, fill = Me
   facet_wrap(facets = vars(Model))
 ggplotly(all_model_heatmap, tooltip = "text")
 ggsave("all_model_heatmap.png", all_model_heatmap, width=10, height=10, dpi=300)                        
+
+
+
+all_model_barplot <- ggplot(model_results, aes(x=DataSetId, fill=FSM, y=Mean_AUC)) +
+  geom_bar(stat="identity", position="dodge") +
+  geom_errorbar( aes(x=DataSetId, ymin=X95.CI_AUC_lower, ymax=X95.CI_AUC_upper), position="dodge") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1)) +
+  facet_wrap(facets = vars(Model))  
+ggsave("all_model_barplot.png", all_model_barplot, width=10, height=10, dpi=300)
