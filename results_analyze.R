5986fce85096df17fa3eca8fe46f7182b60aa3f1
abhivij <abhishek.kcvijayan@gmail.com>
2020-10-26 22:38:45 +1100

    added plots, added plots for features count and cumvar component count

--- results_analyze.R
+++ results_analyze.R
@@ -87,4 +87,89 @@ features_barplot_nonTEPS <- ggplot(non_TEP_fsm_info, aes(x=DataSetId, fill=FSM, 
   scale_y_log10()
 ggsave("features_count_barplot_non_TEPS.png", features_barplot_nonTEPS, width=10, height=10, dpi=300)
 
+
+transformation_fsm_info1 <- transformation_fsm_info %>%
+  select(-c(starts_with("X")))
+
+transformation_fsm_info_long1 <- gather_(data = transformation_fsm_info1,
+                                        key_col = 'CumVar', 
+                                        value_col = 'NumComponents',
+                                        gather_cols = c('Mean_Number.of.components.for.Cum.Var.0.5', 
+                                                        'Mean_Number.of.components.for.Cum.Var.0.75', 
+                                                        'Mean_Number.of.components.for.Cum.Var.0.9',  
+                                                        'Mean_Number.of.components.for.Cum.Var.0.95', 
+                                                        'Mean_Number.of.components.for.Cum.Var.0.99'))
+transformation_fsm_info_long1 <- transformation_fsm_info_long1 %>%
+  mutate(CumVar = sub("Mean_Number.of.components.for.Cum.Var.", "", CumVar)) %>%
+  mutate(CumVar = factor(CumVar)) %>%
+  arrange(DataSetId, FSM)
+
+
+transformation_fsm_info2 <- transformation_fsm_info %>%
+  select(-c(starts_with("Mean"), ends_with("lower")))
+transformation_fsm_info_long2 <- gather_(data = transformation_fsm_info2,
+                                        key_col = 'CumVar', 
+                                        value_col = 'NumComponents95CIU',
+                                        gather_cols = c('X95.CI_Number.of.components.for.Cum.Var.0.5_upper', 
+                                                        'X95.CI_Number.of.components.for.Cum.Var.0.75_upper', 
+                                                        'X95.CI_Number.of.components.for.Cum.Var.0.9_upper',  
+                                                        'X95.CI_Number.of.components.for.Cum.Var.0.95_upper', 
+                                                        'X95.CI_Number.of.components.for.Cum.Var.0.99_upper'))
+transformation_fsm_info_long2 <- transformation_fsm_info_long2 %>%
+  mutate(CumVar = sub("X95.CI_Number.of.components.for.Cum.Var.", "", CumVar)) %>%
+  mutate(CumVar = sub("_upper", "", CumVar)) %>%
+  mutate(CumVar = factor(CumVar)) %>%
+  arrange(DataSetId, FSM)
+  
+
+  
+transformation_fsm_info3 <- transformation_fsm_info %>%
+  select(-c(starts_with("Mean"), ends_with("upper")))
+transformation_fsm_info_long3 <- gather_(data = transformation_fsm_info3,
+                                         key_col = 'CumVar', 
+                                         value_col = 'NumComponents95CIL',
+                                         gather_cols = c('X95.CI_Number.of.components.for.Cum.Var.0.5_lower', 
+                                                         'X95.CI_Number.of.components.for.Cum.Var.0.75_lower', 
+                                                         'X95.CI_Number.of.components.for.Cum.Var.0.9_lower',  
+                                                         'X95.CI_Number.of.components.for.Cum.Var.0.95_lower', 
+                                                         'X95.CI_Number.of.components.for.Cum.Var.0.99_lower'))
+transformation_fsm_info_long3 <- transformation_fsm_info_long3 %>%
+  mutate(CumVar = sub("X95.CI_Number.of.components.for.Cum.Var.", "", CumVar)) %>%
+  mutate(CumVar = sub("_lower", "", CumVar)) %>%
+  mutate(CumVar = factor(CumVar)) %>%
+  arrange(DataSetId, FSM)
+
+transformation_fsm_info <- transformation_fsm_info_long1 %>%
+  inner_join(transformation_fsm_info_long2, by = c("DataSetId", "FSM", "CumVar")) %>%
+  inner_join(transformation_fsm_info_long3, by = c("DataSetId", "FSM", "CumVar"))
+
+transformation_cumvar_barplot <- ggplot(transformation_fsm_info, aes(x=DataSetId, fill=CumVar, y=NumComponents)) +
+  geom_bar(stat="identity", position="dodge") +
+  geom_errorbar( aes(x=DataSetId, ymin=NumComponents95CIL, ymax=NumComponents95CIU), position="dodge") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1)) +
+  scale_y_log10() +
+  facet_wrap(facets = vars(FSM))    
+ggsave("transformation_cumvar_barplot.png", transformation_cumvar_barplot, width=10, height=10, dpi=300)
+  
+
+TEP_transformation_fsm_info <- transformation_fsm_info %>%
+  filter(DataSetId %in% TEP_datasets)
+non_TEP_transformation_fsm_info <- transformation_fsm_info %>%
+  filter(! DataSetId %in% TEP_datasets)
+
+transformation_cumvar_barplot_TEPS <- ggplot(TEP_transformation_fsm_info, aes(x=DataSetId, fill=CumVar, y=NumComponents)) +
+  geom_bar(stat="identity", position="dodge") +
+  geom_errorbar( aes(x=DataSetId, ymin=NumComponents95CIL, ymax=NumComponents95CIU), position="dodge") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1)) +
+  facet_wrap(facets = vars(FSM))    
+ggsave("transformation_cumvar_barplot_TEPS.png", transformation_cumvar_barplot_TEPS, width=10, height=10, dpi=300)
+
+
+transformation_cumvar_barplot_non_TEPS <- ggplot(non_TEP_transformation_fsm_info, aes(x=DataSetId, fill=CumVar, y=NumComponents)) +
+  geom_bar(stat="identity", position="dodge") +
+  geom_errorbar( aes(x=DataSetId, ymin=NumComponents95CIL, ymax=NumComponents95CIU), position="dodge") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1)) +
+  facet_wrap(facets = vars(FSM))    
+ggsave("transformation_cumvar_barplot_non_TEPS.png", transformation_cumvar_barplot_non_TEPS, width=10, height=10, dpi=300)
+
 #plots from fsm_info.csv end
