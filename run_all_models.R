ad431b09cf0c8b1a07a5a0a47469e9350fccef39
abhivij <abhishek.kcvijayan@gmail.com>
2020-11-09 19:35:54 +1100

    added l1 logreg

--- run_all_models.R
+++ run_all_models.R
@@ -6,7 +6,8 @@ run_all_models <- function(x.train, y.train, x.test, y.test, classes = classes){
   
   results <- list(
     logistic_regression(x.train, y.train, x.test, y.test, classes = classes),
-    logistic_regression(x.train, y.train, x.test, y.test, classes = classes, regularize = TRUE),
+    logistic_regression(x.train, y.train, x.test, y.test, classes = classes, regularize = 'l1'),
+    logistic_regression(x.train, y.train, x.test, y.test, classes = classes, regularize = 'l2'),
     svm_model(x.train, y.train, x.test, y.test, classes = classes),
     svm_model(x.train, y.train, x.test, y.test, classes = classes, kernel = 'radial'),
     rf_model(x.train, y.train, x.test, y.test, classes = classes)
