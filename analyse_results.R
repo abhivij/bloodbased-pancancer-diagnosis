8b3fbe9b4c599fc71c95c7d34557ff1212a57c7f
abhivij <abhishek.kcvijayan@gmail.com>
2020-11-15 03:21:23 +1100

    all cd diagrams in one plot

--- analyse_results.R
+++ analyse_results.R
@@ -24,27 +24,31 @@ all_model_results <- all_model_results %>%
   filter(FSM %in% c('all', 't-test', 'wilcoxontest', 'PCA_90', 'RF_RFE')) %>%
   select(DataSetId, FSM, Model, Mean_AUC)
 
-model <- 'Random Forest'
-model_results <- all_model_results %>%
-  filter(Model == model) %>%
-  select(-Model)
+par(mfrow=c(3,2))
+for (model in unique(all_model_results$Model)){
+  model_results <- all_model_results %>%
+    filter(Model == model) %>%
+    select(-Model)
+  
+  model_results_fsm <- pivot_wider(model_results, names_from = FSM, values_from = Mean_AUC)
+  model_results_fsm <- column_to_rownames(model_results_fsm, var = 'DataSetId')
+  
+  avg_ranks <- colMeans(rankMatrix(model_results_fsm))
+  
+  friedman_test <- friedmanTest(model_results_fsm)
+  
+  corrected_friedman_test <- imanDavenportTest(model_results_fsm)
+  
+  nemenyi_test <- nemenyiTest(model_results_fsm, alpha=0.05)
+  print(nemenyi_test$diff.matrix)
+  plotCD(model_results_fsm)
+  title(model)
+  
+  model_results_dataset <- pivot_wider(model_results, names_from = DataSetId, values_from = Mean_AUC)
+  model_results_dataset <- column_to_rownames(model_results_dataset, var = 'FSM')
+  print(kendall.w(model_results_dataset))
+}
 
-model_results_fsm <- pivot_wider(model_results, names_from = FSM, values_from = Mean_AUC)
-model_results_fsm <- column_to_rownames(model_results_fsm, var = 'DataSetId')
 
-avg_ranks <- colMeans(rankMatrix(model_results_fsm))
-
-friedman_test <- friedmanTest(model_results_fsm)
-
-corrected_friedman_test <- imanDavenportTest(model_results_fsm)
-
-nemenyi_test <- nemenyiTest(model_results_fsm, alpha=0.05)
-print(nemenyi_test$diff.matrix)
-plotCD(model_results_fsm)
-
-
-model_results_dataset <- pivot_wider(model_results, names_from = DataSetId, values_from = Mean_AUC)
-model_results_dataset <- column_to_rownames(model_results_dataset, var = 'FSM')
-print(kendall.w(model_results_dataset))
 
 
