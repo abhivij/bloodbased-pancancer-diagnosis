b160d5dc7b27f5444d1cfd30fca2801198dd28f3
abhivij <abhishek.kcvijayan@gmail.com>
2020-11-17 23:14:09 +1100

    no repeatedcv - using default

--- feature_selection/rfrfe.R
+++ feature_selection/rfrfe.R
@@ -21,15 +21,17 @@ generate_powers = function(limit, base = 2){
 rfrfe <- function(x.train, y.train, x.test, y.test, classes, random_seed = 1000, ...){
   set.seed(random_seed)
   
-  ctrl <- rfeControl(functions = rfFuncs,
-                     method = "repeatedcv")
+  start_time <- Sys.time()
   
+  ctrl <- rfeControl(functions = rfFuncs)
   rfe_with_rf <- rfe(x.train, y.train$Label,
                    sizes = generate_powers(dim(x.train)[2]),
                    rfeControl = ctrl)
+
+  end_time <- Sys.time()
+  print(end_time - start_time)
   
   features <- predictors(rfe_with_rf)
-
   x.train <- x.train[, features, drop = FALSE]
   x.test <- x.test[, features, drop = FALSE]  
     
