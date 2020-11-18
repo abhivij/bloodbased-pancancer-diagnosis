69fbfe296cba00367b8c1488b62ff375d55435c3
abhivij <abhishek.kcvijayan@gmail.com>
2020-11-18 20:17:20 +1100

    correcting arg number

--- pipeline_executor.R
+++ pipeline_executor.R
@@ -3,14 +3,14 @@ source("pipeline.R")
 source("dataset_pipeline_arguments.R")
 
 args = commandArgs(trailingOnly = TRUE)
-if (length(args) == 0) {
+if (length(args) > 1) {
   print('Executing pipeline on all datasets')
   for (dparg in dataset_pipeline_arguments) {
     do.call(execute_pipeline, dparg)
   }
 } else {
-  print(paste('Executing pipeline on dataset', args[1]))
-  dparg = dataset_pipeline_arguments[[args[1]]]
+  print(paste('Executing pipeline on dataset', args[2]))
+  dparg = dataset_pipeline_arguments[[args[2]]]
   do.call(execute_pipeline, dparg)
 }
 
