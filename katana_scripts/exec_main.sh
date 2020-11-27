8b892c00fab3db4d9261e4c34a53f5718681fbc2
abhivij <abhishek.kcvijayan@gmail.com>
2020-11-27 21:01:24 +1100

    separate job for dataset15

--- katana_scripts/exec_main.sh
+++ katana_scripts/exec_main.sh
@@ -4,4 +4,4 @@ cd $HOME/bloodbased-pancancer-diagnosis/katana_scripts
 
 qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 1-11 exec.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=100:00:00 -J 12-14 exec.pbs
-qsub -l select=1:ncpus=16:mem=124gb,walltime=200:00:00 -J 15-15 exec.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=200:00:00 exec15.pbs
