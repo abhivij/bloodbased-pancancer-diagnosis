b9f4a27858c4505ee6cc07492a3af3d985df73ae
abhivij <abhishek.kcvijayan@gmail.com>
2021-01-08 22:02:34 +1100

    rf features fem

--- feature_extraction_arguments.R
+++ feature_extraction_arguments.R
@@ -3,6 +3,7 @@ source("feature_extraction/wilcoxon_test.R")
 source("feature_extraction/rfrfe.R")
 source("feature_extraction/ga.R")
 source("feature_extraction/pca.R")
+source("feature_extraction/rf_features.R")
 source("feature_extraction/ranger_features.R")
 source("feature_extraction/mrmr_features.R")
 source("feature_extraction/phate_transformation.R")
@@ -26,6 +27,7 @@ feature_extraction_arguments <- list(
   # list(fsm = wilcoxon_test_features, fsm_name = "wilcoxontest_BY", adjust_method = 'BY'),
   # list(fsm = rfrfe, fsm_name = "RF_RFE"),
   # list(fsm = ga, fsm_name = "ga_rf"),
+  list(fsm = rf_features, fsm_name = "rf")
   # list(fsm = ranger_features, fsm_name = "ranger_perm", imp = "permutation")
   # list(fsm = ranger_features, fsm_name = "ranger_impu", imp = "impurity"),
   # list(fsm = ranger_features, fsm_name = "ranger_impu_cor", imp = "impurity_corrected"),
@@ -39,27 +41,27 @@ feature_extraction_arguments <- list(
   # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "PCA_95", variance_threshold = 0.95),
   # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "PCA_99", variance_threshold = 0.99),
   # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "PCA_100"),
-  list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca2", embedding_size = 2),
-  list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca5", embedding_size = 5),
-  list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca2", embedding_size = 2),
+  # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca5", embedding_size = 5),
+  # list(transformation = TRUE, fsm = pca_transformation, fsm_name = "pca_var", var_embedding = TRUE),
   # list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate2", embedding_size = 2),
   # list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate5", embedding_size = 5),
-  list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate_var", var_embedding = TRUE),
-  list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate_pcavar", var_embedding = TRUE, use_pca = TRUE),
+  # list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = phate_transformation, fsm_name = "phate_pcavar", var_embedding = TRUE, use_pca = TRUE),
   # list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap2", embedding_size = 2),
   # list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap5", embedding_size = 5),
-  list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap_var", var_embedding = TRUE),
-  list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap_pcavar", var_embedding = TRUE, use_pca = TRUE),
+  # list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = umap_transformation, fsm_name = "umap_pcavar", var_embedding = TRUE, use_pca = TRUE),
   # list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda2", embedding_size = 2),
   # list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda5", embedding_size = 5),
-  list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda_var", var_embedding = TRUE),
-  list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda_pcavar", var_embedding = TRUE, use_pca = TRUE),
+  # list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = plsda_transformation, fsm_name = "plsda_pcavar", var_embedding = TRUE, use_pca = TRUE),
   # list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca_all"),
   # list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca2", embedding_size = 2),
   # list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca5", embedding_size = 5),
-  list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca_var", var_embedding = TRUE),
-  list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca_pcavar", var_embedding = TRUE, use_pca = TRUE),
+  # list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = kpca_transformation, fsm_name = "kpca_pcavar", var_embedding = TRUE, use_pca = TRUE),
   # list(transformation = TRUE, fsm = mrmr_plsda_transformation, fsm_name = "mrmr_plsda", embedding_size = 5),
-  list(transformation = TRUE, fsm = mrmr_plsda_transformation, fsm_name = "mrmr_plsda_var", var_embedding = TRUE),
-  list(transformation = TRUE, fsm = mrmr_plsda_transformation, fsm_name = "mrmr_plsda_pcavar", var_embedding = TRUE, use_pca = TRUE)
+  # list(transformation = TRUE, fsm = mrmr_plsda_transformation, fsm_name = "mrmr_plsda_var", var_embedding = TRUE),
+  # list(transformation = TRUE, fsm = mrmr_plsda_transformation, fsm_name = "mrmr_plsda_pcavar", var_embedding = TRUE, use_pca = TRUE)
 )
