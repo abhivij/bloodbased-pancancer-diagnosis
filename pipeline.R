7c62ef3eac20aa9ec8d48d12dd69b9954c694a57
abhivij <abhishek.kcvijayan@gmail.com>
2021-09-21 21:17:59 +1000

    to run all FEMs for all datasets

--- pipeline.R
+++ pipeline.R
@@ -13,7 +13,7 @@ execute_pipeline <- function(phenotype_file_name,
                              extracted_count_file_name = "read_counts.txt",
                              output_label_file_name = "output_labels.txt",
                              dataset_id, cores = 16,
-                             results_dir_path = "results_breastcanceronly"){
+                             results_dir_path = "results"){
   start_time <- Sys.time()
   print(paste("Pipeline Execution on", dataset_id, classification_criteria))
   
