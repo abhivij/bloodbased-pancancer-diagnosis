83158e47b4c41344239cdfad2307f4857539f3b0
abhivij <abhishek.kcvijayan@gmail.com>
2021-11-08 17:14:39 +1100

    execute exec25

--- katana_scripts/exec_main.sh
+++ katana_scripts/exec_main.sh
@@ -5,5 +5,6 @@ cd $HOME/bloodbased-pancancer-diagnosis/katana_scripts
 qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 1-11 exec.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=100:00:00 -J 12-14 exec.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=200:00:00 -J 15-16 exec.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=200:00:00 exec25.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=100:00:00 -J 17-22 exec.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=200:00:00 -J 23-24 exec.pbs
