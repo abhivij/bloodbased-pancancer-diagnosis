9b3ebb5dcd05503e2af252e82576182c1db13a0c
abhivij <abhishek.kcvijayan@gmail.com>
2021-11-16 21:56:51 +1100

    analyse features update

--- results_processing/analyse_results.R
+++ results_processing/analyse_results.R
@@ -45,6 +45,11 @@ analyse_FEM_results <- function(all_model_results, dir_path = "CD", comparison_c
       filter(DataSetId %in% dataset_filter)
   }
   
+  if(!"ranger_impu" %in% unique(all_model_results$FSM)){
+    all_model_results <- all_model_results %>%
+      mutate(FSM = gsub("ranger_impu_cor", "ranger", FSM))
+  }
+  
   for (model in unique(all_model_results$Model)){
     model_results <- all_model_results %>%
       filter(Model == model) %>%
@@ -62,13 +67,14 @@ analyse_FEM_results <- function(all_model_results, dir_path = "CD", comparison_c
     nemenyi_test <- nemenyiTest(model_results_fsm, alpha=0.05)
     print(nemenyi_test$diff.matrix)
 
-    cd_filename <- str_replace_all(paste(model, 'CD.svg', sep = '_'), " ", "_")
+    cd_filename <- str_replace_all(paste(model, 'CD.pdf', sep = '_'), " ", "_")
 
-    plotCD(model_results_fsm, cex = 1)
-    title(model)
+    plotCD(model_results_fsm, cex = 3)
+    # title(model)
+    
 
-    dev.copy(svg, filename = get_file_path(cd_filename, dir_path),
-             width = plot_width, height = 5)
+    dev.copy(pdf, get_file_path(cd_filename, dir_path),
+             width = plot_width, height = 10)
     dev.off()
 
     model_results_dataset <- pivot_wider(model_results, names_from = DataSetId, values_from = comparison_criteria)
@@ -80,19 +86,19 @@ analyse_FEM_results <- function(all_model_results, dir_path = "CD", comparison_c
 
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/Mean_AUC",
-                    comparison_criteria = "Mean_AUC")
+                    comparison_criteria = "Mean_AUC", plot_width = 33)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/AUC95CIdiff",
-                    comparison_criteria = "AUC95CIdiff")
+                    comparison_criteria = "AUC95CIdiff", plot_width = 33)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/X95.CI_AUC_lower",
-                    comparison_criteria = "X95.CI_AUC_lower")
+                    comparison_criteria = "X95.CI_AUC_lower", plot_width = 33)
 
 #var
 analyse_FEM_results(all_model_results = all_model_results,
-                    dir_path = "CD/var/Mean_AUC",
+                    dir_path = "CD/var_Mean_AUC",
                     comparison_criteria = "Mean_AUC",
-                    fem_allowed = fem_vector_var)
+                    fem_allowed = fem_vector_var, plot_width = 33)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/var/AUC95CIdiff",
                     comparison_criteria = "AUC95CIdiff",
@@ -105,9 +111,9 @@ analyse_FEM_results(all_model_results = all_model_results,
 
 #pcavar
 analyse_FEM_results(all_model_results = all_model_results,
-                    dir_path = "CD/pca_var/Mean_AUC",
+                    dir_path = "CD/pca_var_Mean_AUC",
                     comparison_criteria = "Mean_AUC",
-                    fem_allowed = fem_vector_pcavar)
+                    fem_allowed = fem_vector_pcavar, plot_width = 33)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/pca_var/AUC95CIdiff",
                     comparison_criteria = "AUC95CIdiff",
@@ -119,15 +125,15 @@ analyse_FEM_results(all_model_results = all_model_results,
 
 
 analyse_FEM_results(all_model_results = all_model_results,
-                    dir_path = "CD/filter_compare/Mean_AUC",
+                    dir_path = "CD/filter_compare_Mean_AUC",
                     comparison_criteria = "Mean_AUC",
-                    fem_allowed = fem_vector_fil_compare, plot_width = 18)
+                    fem_allowed = fem_vector_fil_compare, plot_width = 40)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/filter_compare/AUC95CIdiff",
                     comparison_criteria = "AUC95CIdiff",
-                    fem_allowed = fem_vector_fil_compare, plot_width = 18)
+                    fem_allowed = fem_vector_fil_compare, plot_width = 40)
 analyse_FEM_results(all_model_results = all_model_results,
                     dir_path = "CD/filter_compare/X95.CI_AUC_lower",
                     comparison_criteria = "X95.CI_AUC_lower",
-                    fem_allowed = fem_vector_fil_compare, plot_width = 18)
+                    fem_allowed = fem_vector_fil_compare, plot_width = 40)
 
