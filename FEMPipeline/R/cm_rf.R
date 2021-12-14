c50a51a98c72c2960bc48086967cd4683fc9ccff
abhivij <abhishek.kcvijayan@gmail.com>
2021-12-14 16:48:03 +1100

    bugfix : handling special case in RF

--- FEMPipeline/R/cm_rf.R
+++ FEMPipeline/R/cm_rf.R
@@ -12,20 +12,27 @@ rf_model <- function(x.train, y.train, x.test, y.test, classes,
   
   try({
     set.seed(random_seed)
-    model <- randomForest::randomForest(x = x.train, y = factor(y.train$Label, levels = classes))
-    
-    if(classifier_feature_imp){
-      feature_imp <- data.frame(randomForest::importance(model))
-      feature_imp <- cbind(feature = rownames(feature_imp),
-                           data.frame(feature_imp, row.names = NULL))
+    if(sum(x.train - colMeans(x.train)) != 0){
+      #ensure that atleast one column is not a constant vector
+      #all columns constant causes the below line to run forever
+      model <- randomForest::randomForest(x = x.train, y = factor(y.train$Label, levels = classes))
+
+      if(classifier_feature_imp){
+        feature_imp <- data.frame(randomForest::importance(model))
+        feature_imp <- cbind(feature = rownames(feature_imp),
+                             data.frame(feature_imp, row.names = NULL))
+      }
+
+      pred_prob <- predict(model, x.test, type="prob")
+      pred_prob <- data.frame(pred_prob)[classes[2]]
+      pred <- ifelse(pred_prob > 0.5, classes[2], classes[1])
+
+      metrics <- compute_metrics(pred = pred, pred_prob = pred_prob, true_label = y.test$Label, classes = classes)
+    } else{
+      print("data to RF : all fields constant!")
+      print(dim(x.train))
     }
     
-    
-    pred_prob <- predict(model, x.test, type="prob")
-    pred_prob <- data.frame(pred_prob)[classes[2]]
-    pred <- ifelse(pred_prob > 0.5, classes[2], classes[1])
-    
-    metrics <- compute_metrics(pred = pred, pred_prob = pred_prob, true_label = y.test$Label, classes = classes)    
   })
   
   return (list(model_name, metrics, feature_imp))
