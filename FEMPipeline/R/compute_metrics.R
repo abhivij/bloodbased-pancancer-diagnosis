9306675e58f3873b0a7d9d403467ab76bb508573
abhivij <abhishek.kcvijayan@gmail.com>
2022-05-13 22:16:24 +1000

    auc using caret

--- FEMPipeline/R/compute_metrics.R
+++ FEMPipeline/R/compute_metrics.R
@@ -19,25 +19,6 @@ compute_metrics <- function(pred, pred_prob, true_label, classes){
   # ?ROCR::performance
   # ROCR::performance(pr, measure = "f")@y.values[[1]]
   
-  
-  caret::auc
-  
-  
-  set.seed(144)
-  true_class <- factor(sample(paste0("Class", 1:2), 
-                              size = 1000,
-                              prob = c(.2, .8), replace = TRUE))
-  true_class <- sort(true_class)
-  class1_probs <- rbeta(sum(true_class == "Class1"), 4, 1)
-  class2_probs <- rbeta(sum(true_class == "Class2"), 1, 2.5)
-  test_set <- data.frame(obs = true_class,
-                         Class1 = c(class1_probs, class2_probs))
-  test_set$Class2 <- 1 - test_set$Class1
-  test_set$pred <- factor(ifelse(test_set$Class1 >= .5, "Class1", "Class2"))
-  
-  caret::twoClassSummary(test_set, lev = levels(test_set$obs))
-  caret::prSummary(test_set, lev = levels(test_set$obs))
-  
   caret::confusionMatrix(factor(pred), factor(true_label), positive = as.character(classes[2]))
   caret::confusionMatrix(factor(pred), factor(true_label), positive = as.character(classes[2]),
                          mode = "prec_recall")
@@ -52,6 +33,20 @@ compute_metrics <- function(pred, pred_prob, true_label, classes){
   npv <- caret::negPredValue(factor(pred), factor(true_label), negative = classes[1])
   
   caret::F_meas(data = factor(pred), reference = factor(true_label), relevant = as.character(classes[2]))
+  
+  result_set_for_caret <- data.frame(
+    #caret requires class1 to be positive and class2 negative
+    #classes argument in this compute_metrics function contains c(negative_class, positive_class)
+    "obs" = factor(dplyr::case_when(true_label == classes[2] ~ "Class1",
+                      true_label == classes[1] ~ "Class2")),
+    "Class1" = pred_prob,
+    "Class2" = 1-pred_prob,
+    "pred" = factor(dplyr::case_when(pred == classes[2] ~ "Class1",
+                       pred == classes[1] ~ "Class2"))
+  )
+  colnames(result_set_for_caret) <- c("obs", "Class1", "Class2", "pred")
+  caret::twoClassSummary(result_set_for_caret, lev = levels(result_set_for_caret$obs))
+  caret::prSummary(result_set_for_caret, lev = levels(result_set_for_caret$obs))
   
   return (c(acc, auc, tpr, tnr))
 }
