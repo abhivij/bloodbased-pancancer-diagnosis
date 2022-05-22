74ec9e77a156dc16ad04a9e754527192832ec537
abhivij <abhishek.kcvijayan@gmail.com>
2022-05-22 22:03:14 +1000

    check rna types complete

--- explore.R
+++ explore.R
@@ -1,79 +1,26 @@
 library(tidyverse)
+library(biomaRt)
 
 setwd("~/UNSW/VafaeeLab/bloodbased-pancancer-diagnosis/")
 
-data <- read.table("data/GSE71008/GSE71008_Data_matrix.txt", header=TRUE, row.names=1,
-                   skip=0, nrows=-1, comment.char="", fill=TRUE)
+# data <- read.table("data/GSE71008/GSE71008_Data_matrix.txt", header=TRUE, row.names=1,
+#                    skip=0, nrows=-1, comment.char="", fill=TRUE)
+# RNA types provided in https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71008
 
-data <- read.table("data/GBM/TEP/GSE68086_TEP_data_matrix.txt", header=TRUE, row.names=1,
-                   skip=0, nrows=-1, comment.char="", fill=TRUE)
-
-data <- read.table("data/LungCancer/TEP/GSE89843_TEP_Count_Matrix.txt", header=TRUE, row.names=1,
-                   skip=0, nrows=-1, comment.char="", fill=TRUE)
-
-library(org.Hs.eg.db)
-
-keytypes(org.Hs.eg.db)
-
-columns(org.Hs.eg.db)
-
-eKeys <- head(keys(org.Hs.eg.db, keytype="ENSEMBL"))
-cols <- c("GENENAME", "GENETYPE", "SYMBOL")
-select(org.Hs.eg.db, keys=eKeys, columns=cols, keytype="ENSEMBL")
-
-select(org.Hs.eg.db, keys= c("ENSG00000249684", "ENSG00000267279", "ENSG00000273143", "ENSG00000283904"), 
-       columns=cols, keytype="ENSEMBL")
-
-ENSG00000249684
-
-
-data <- read.table("data/LungCancer/TEP/GSE89843_TEP_Count_Matrix.txt", header=TRUE, row.names=1,
-                   skip=0, nrows=-1, comment.char="", fill=TRUE)
-gene_type <- select(org.Hs.eg.db, keys= rownames(data), 
-       columns=cols, keytype="ENSEMBL")
-summary(factor(gene_type$GENETYPE))
-
-# ncRNA          other protein-coding         pseudo           NA's 
-#             75              5           4530             31            110 
-
-
-
-data <- read.table("data/GBM/TEP/GSE68086_TEP_data_matrix.txt", header=TRUE, row.names=1,
-                   skip=0, nrows=-1, comment.char="", fill=TRUE)
-gene_type <- select(org.Hs.eg.db, keys= rownames(data), 
-                    columns=cols, keytype="ENSEMBL")
-summary(factor(gene_type$GENETYPE))
-
-# ncRNA          other protein-coding         pseudo           rRNA         snoRNA          snRNA 
-# 5026            334          18968           8578             18            368             28 
-# NA's 
-#          24633 
-
-
-
-library(biomaRt)
-
+# miRNA (~40.4%), 
+# piRNAs (~40.0%),
+# pseudo-genes (~3.7%), 
+# lncRNAs (~2.4%), 
+# tRNAs (~2.1%),
+# mRNAs (~2.1%)
 
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
 head(listAttributes(ensembl))
 
-getBM(
-  attributes = c(
-    'ensembl_gene_id',
-    'ensembl_transcript_id',
-    'hgnc_symbol',
-    'chromosome_name',
-    'start_position',
-    'end_position'
-  ),
-  filters =
-    'chromosome_name',
-  values = "1",
-  mart = ensembl
-)
+data <- read.table("data/GBM/TEP/GSE68086_TEP_data_matrix.txt", header=TRUE, row.names=1,
+                   skip=0, nrows=-1, comment.char="", fill=TRUE)
 
-
-getBM(
+rna_types <- getBM(
   attributes = c(
     'ensembl_gene_id',
     'ensembl_transcript_id',
@@ -84,6 +31,57 @@ getBM(
   ),
   filters =
     'ensembl_gene_id',
-  values = c("ENSG00000249684", "ENSG00000267279", "ENSG00000273143", "ENSG00000283904", "ENSG00000000419"),
+  values = rownames(data),
   mart = ensembl
 )
+summary(factor(rna_types$gene_biotype))
+summary(factor(rna_types$transcript_biotype))
+
+options(scipen=999)
+rna_types_df <- data.frame(count = summary(factor(rna_types$gene_biotype))) %>%
+  rownames_to_column("gene_biotype")
+rna_types_df <- rna_types_df %>%
+  mutate(per = count * 100/ sum(count)) %>%
+  arrange(desc(per))
+
+
+
+data <- read.table("data/LungCancer/TEP/GSE89843_TEP_Count_Matrix.txt", header=TRUE, row.names=1,
+                   skip=0, nrows=-1, comment.char="", fill=TRUE)
+rna_types_tep2017 <- getBM(
+  attributes = c(
+    'ensembl_gene_id',
+    'ensembl_transcript_id',
+    'gene_biotype',
+    'transcript_biotype',
+    'source',
+    'transcript_source'
+  ),
+  filters =
+    'ensembl_gene_id',
+  values = rownames(data),
+  mart = ensembl
+)
+summary(factor(rna_types_tep2017$gene_biotype))
+rna_types_tep2017_df <- data.frame(count = summary(factor(rna_types_tep2017$gene_biotype))) %>%
+  rownames_to_column("gene_biotype")
+rna_types_tep2017_df <- rna_types_tep2017_df %>%
+  mutate(per = count * 100/ sum(count)) %>%
+  arrange(desc(per))
+
+
+
+#considering those with > 1%
+
+# TEP2015
+# 
+# mRNAs (~70.5%),
+# lncRNAs (~19%),
+# pseudo-genes (~7.5%)
+
+
+
+# TEP2017
+# 
+# mRNAs (~96.8%),
+# lncRNAs (~2.7%)
