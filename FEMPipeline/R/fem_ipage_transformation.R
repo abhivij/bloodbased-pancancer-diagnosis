b52701255f01d8ac8f3a49e0e7e0eed807be4535
abhivij <abhishek.kcvijayan@gmail.com>
2022-06-30 12:37:16 +1000

    pval via apply

--- FEMPipeline/R/fem_ipage_transformation.R
+++ FEMPipeline/R/fem_ipage_transformation.R
@@ -4,7 +4,8 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
                                  adj_method = "bonferroni", p_cutoff = 1e-16,  
                                  ...){
   
-  
+  adj_method = "bonferroni"
+  p_cutoff = 1e-16
   classes <- c("Control", "GBM")
   x.train <- read.csv("dparg70_xtrain.csv", row.names = 1)
   x.test <- read.csv("dparg70_xtest.csv", row.names = 1)
@@ -20,7 +21,9 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
   dimnames(result)[[1]] <- colnames(x.train)
   dimnames(result)[[2]] <- colnames(x.train)
   result
+  dim(result)
   
+  start_time <- Sys.time()
   for(r in c(1:nrow(x.train))){
     row_result <- apply(combn(colnames(x.train), 2), MARGIN = 2, 
                         FUN = function(c) {
@@ -31,25 +34,37 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
     )  
     result[, , r][upper.tri(result[, , r])] <- row_result
   }
-  result
+  end_time <- Sys.time()
+  print(end_time - start_time) 
+  
+  print(dim(result))
+  
   
   
   #determine relevant feature combinations
-  for(i in c(1:(ncol(m)-1))){
-    for(j in c((i+1):ncol(m))){
-      sum(result[1, 2, ])
-      
-      result[1, 2, ] > 0 & y.train$Label == classes[1]
-      result[1, 2, ] < 0 & y.train$Label == classes[1]
-      
-      result[1, 2, ] > 0 & y.train$Label == classes[2]
-      result[1, 2, ] < 0 & y.train$Label == classes[2]
-      
-      a <- result[i, j, ]
-    }
-  }
+  start_time <- Sys.time()
+  p_val <- apply(combn(colnames(x.train), 2), MARGIN = 2, 
+        FUN = function(c) {
+          i <- c[1]
+          j <- c[2]
+          a <- sum(result[i, j, ] > 0 & y.train$Label == classes[1])
+          b <- sum(result[i, j, ] < 0 & y.train$Label == classes[1])
+          
+          c <- sum(result[i, j, ] > 0 & y.train$Label == classes[2])
+          d <- sum(result[i, j, ] < 0 & y.train$Label == classes[2])
+          
+          p <- (ncol(combn(a+b, a)) * ncol(combn(c+d, c)))/ncol(combn(a+b+c+d, a+c))
+          
+          return(rbind(i, j, p))
+        }
+  )  
+  end_time <- Sys.time()
+  print(end_time - start_time) 
   
+  p_val <- t(p_val)
+  p_val[,3] <- p.adjust(p_val[,3], method = adj_method)
   
+  ######
   
   plsda_transform <- caret::plsda(x.train, factor(y.train$Label), ncomp = 2)
   
