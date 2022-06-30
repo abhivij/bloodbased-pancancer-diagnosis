a44bcaf0aa360eb245294189a019b5c1ea828939
abhivij <abhishek.kcvijayan@gmail.com>
2022-06-30 13:14:26 +1000

    -1, 1 and transform

--- FEMPipeline/R/fem_ipage_transformation.R
+++ FEMPipeline/R/fem_ipage_transformation.R
@@ -6,22 +6,24 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
   
   adj_method = "bonferroni"
   p_cutoff = 1e-16
+  p_cutoff = 0.05
   classes <- c("Control", "GBM")
   x.train <- read.csv("dparg70_xtrain.csv", row.names = 1)
   x.test <- read.csv("dparg70_xtest.csv", row.names = 1)
   y.train <- read.csv("dparg70_ytrain.csv", row.names = 1)
   y.test <- read.csv("dparg70_ytest.csv", row.names = 1)
   
-  x.train <- x.train[c(1:3), c(1:10)]
-  y.train <- y.train[c(1:3), ]
+  # x.train <- x.train[c(1:10), c(1:100)]
+  # y.train <- y.train[c(1:10), ]
   
   
   
   result <- array(dim = c(ncol(x.train), ncol(x.train), nrow(x.train)))
   dimnames(result)[[1]] <- colnames(x.train)
   dimnames(result)[[2]] <- colnames(x.train)
-  result
+  #result
   dim(result)
+  
   
   start_time <- Sys.time()
   for(r in c(1:nrow(x.train))){
@@ -29,7 +31,12 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
                         FUN = function(c) {
                           c1 <- c[1]
                           c2 <- c[2]
-                          return(x.train[r, c1] - x.train[r, c2])  
+                          diff <- x.train[r, c1] - x.train[r, c2] 
+                          if(diff >= 0){
+                            diff <- 1
+                          } else{
+                            diff <- -1
+                          }
                         }
     )  
     result[, , r][upper.tri(result[, , r])] <- row_result
@@ -61,8 +68,34 @@ ipage_transformation <- function(x.train, y.train, x.test, y.test,
   end_time <- Sys.time()
   print(end_time - start_time) 
   
+  start_time <- Sys.time()
   p_val <- t(p_val)
   p_val[,3] <- p.adjust(p_val[,3], method = adj_method)
+  end_time <- Sys.time()
+  print(end_time - start_time) 
+  
+  # p_val[c(1, 2, 10:15), 3] <- 0.001
+  
+  imp_feature_pairs <- p_val[p_val[,3] < p_cutoff, c(1:2)]
+  
+  transf.train <- data.frame()
+  for(r in c(1:nrow(x.train))){
+    row_transf <- apply(imp_feature_pairs, MARGIN = 1,
+          FUN = function(c){
+            c1 <- c[1]
+            c2 <- c[2]
+            diff <- x.train[r, c1] - x.train[r, c2]
+            if(diff >= 0){
+              diff <- 1
+            } else{
+              diff <- -1
+            }
+            return(diff)
+          })
+    transf.train <- rbind(transf.train, row_transf)
+  }  
+  colnames(transf.train) <- paste(imp_feature_pairs[,1], imp_feature_pairs[,2], sep = "_")
+  
   
   ######
   
